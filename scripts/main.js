0fd9c5c8314eba04f82ad1d65a5b4dc0e3631e7e
Austin Ko <austin901224@gmail.com>
2021-11-05 09:50:41 +0800

    third update

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,11 @@
-/*let myImage = document.querySelector('img');
+const sectionThree = document.querySelector('.section3');
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/body.jpg') {
-        myImage.setAttribute('src','images/body2.jpg');
-    } else {
-        myImage.setAttribute('src','images/body.jpg')
-    }
-}*/
+const options = {};
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1')
+const observer = new IntersectionObserver(function (entries, observer) {
+	entries.forEach(entry => {
+		console.log(entry);
+	});
+}, options);
 
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozillla is cool, ' + myName;
-}
-
-myButton.onclick = function() {
-    setUserName();
-}
+observer.observe(sectionThree);
